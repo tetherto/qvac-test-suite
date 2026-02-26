14a085c68f33091fa80a12621520156deac0fb7d
Lauri Piisang <lauri.piisang@gmail.com>
2026-02-26 22:20:58 +0200

    add expo-keep-awake to mobile consumer template
    Prevents device from sleeping while test screen is active.
    Calls activateKeepAwakeAsync() at app startup.

--- framework/templates/mobile-consumer/App.tsx
+++ framework/templates/mobile-consumer/App.tsx
@@ -1,7 +1,10 @@
 import 'react-native-url-polyfill/auto';
 import React from 'react';
+import { activateKeepAwakeAsync } from 'expo-keep-awake';
 import BatchConsumer from './batch-consumer';
 // MOBILE_INIT_IMPORT_PLACEHOLDER
+
+activateKeepAwakeAsync();
 
 export default function App() {
   // MOBILE_INIT_REFERENCE_PLACEHOLDER
